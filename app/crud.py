296b081ccc10c1ffd83a1ecd0dfcb1abff168c92
Fahmi Zainal <fahmizainal9@gmail.com>
2024-03-17 17:32:24 +0800

    added feature

--- app/crud.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-from . import models, schemas
-
-def create_phone_number(db: Session, phone_number: schemas.PhoneNumberCreate):
-    db_phone_number = models.PhoneNumber(**phone_number.dict())
-    db.add(db_phone_number)
-    db.commit()
-    db.refresh(db_phone_number)
-    return db_phone_number
