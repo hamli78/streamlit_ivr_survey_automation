a2e1975f089a443ec8c3d7ee7e43f137adac3325
Fahmi Zainal <fahmizainal9@gmail.com>
2024-03-17 17:07:50 +0800

    feat:added feature

---
+++ fastapiapp/app/crud.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Session
+
+from . import models
+from . import schemas
+
+def create_phone_number(db: Session, phone_number: schemas.PhoneNumberCreate):
+    db_phone_number = models.PhoneNumber(**phone_number.dict())
+    db.add(db_phone_number)
+    db.commit()
+    db.refresh(db_phone_number)
+    return db_phone_number
