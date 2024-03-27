919100ff1e7b559af5ee3c1c8bd866f68c8fac31
Fahmi Zainal <fahmizainal9@gmail.com>
2024-03-28 04:00:51 +0800

    modified the import statement,make sure main.py in fastapi same as in test_main

--- fastapiapp/app/main.py
+++ fastapiapp/app/main.py
@@ -1,12 +1,8 @@
 # # #
 from fastapi import FastAPI, HTTPException, UploadFile, File, Form
-from modules.data_cleaner_utils_page1 import (
-    process_file
-)
-from modules.questionnaire_utils_page2 import (parse_questions_and_answers,
-    rename_columns
-)
-from modules.keypress_decoder_utils_page3 import (parse_text_to_json , custom_sort , classify_income , process_file_content, flatten_json_structure)
+from app.modules.data_cleaner_utils_page1 import process_file
+from app.modules.questionnaire_utils_page2 import parse_questions_and_answers, rename_columns
+from app.modules.keypress_decoder_utils_page3 import parse_text_to_json, custom_sort, classify_income, process_file_content, flatten_json_structure
 
 import pandas as pd
 import json
