ba5c63cf01a1a724f69ca1454b9415daac60c506
Fahmi Zainal <fahmizainal9@gmail.com>
2024-03-28 12:51:36 +0800

    added gitigmore

--- fastapiapp/app/tests/test_main.py
+++ fastapiapp/app/tests/test_main.py
@@ -1,10 +1,10 @@
 import pytest
 from fastapi.testclient import TestClient
 from io import BytesIO
-from app.modules.data_cleaner_utils_page1 import process_file
-from app.modules.questionnaire_utils_page2 import parse_questions_and_answers, rename_columns
-from app.modules.keypress_decoder_utils_page3 import parse_text_to_json, custom_sort, classify_income, process_file_content, flatten_json_structure
-
+from modules.data_cleaner_utils_page1 import process_file
+from modules.questionnaire_utils_page2 import parse_questions_and_answers, rename_columns
+from modules.keypress_decoder_utils_page3 import parse_text_to_json, custom_sort, classify_income, process_file_content, flatten_json_structure
+from app.main import app
 import json
 
 client = TestClient(app)
